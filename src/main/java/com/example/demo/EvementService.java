88b7c5dce252a24efc35b681c3fce0770338fbe7
Denguiros <fehmi.denguir@enis.tn>
2021-10-05 19:50:06 +0100

    Initial Commit

---
+++ src/main/java/com/example/demo/EvementService.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.dao.EvenementRepository;
+import com.example.demo.entities.Evenement;
+
+@SpringBootApplication
+public class EvementService implements CommandLineRunner {
+
+	@Autowired
+	EvenementRepository evenementRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvementService.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Evenement evenement = new Evenement();
+		evenementRepository.save(evenement);
+	}
+	
+
+}
